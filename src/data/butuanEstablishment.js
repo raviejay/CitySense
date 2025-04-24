c0692f4436f7dcd57a60f3c210e043b7778b9b0f
Ed Vincent Patulin <patulinedvincent@gmail.com>
2025-04-25 00:54:54 +0800

    Initial commit on establishment branch

--- src/data/butuanEstablishment.js
+++ src/data/butuanEstablishment.js
@@ -12,19 +12,19 @@ export const butuanEstablishments = [
     coords: "8.9479, 125.5438",
   },
   {
-    name: "Caraga state university",
+    name: "Caraga State University - Main Campus",
     coords: "8.960554636112633, 125.59561893959552",
   },
   {
-    name: "Butuan Cathedral",
-    coords: "8.9472, 125.5409",
+    name: "St. Joseph Cathedral - Diocese of Butuan",
+    coords: "8.947746, 125.542842",
   },
   {
     name: "Gaisano Mall Butuan",
     coords: "8.944931447794719, 125.53110445700509",
   },
   {
-    name: "Butuan Airport",
+    name: "Butuan - Bancasi Airport",
     coords: "8.9514, 125.4779",
   },
   {
@@ -33,10 +33,458 @@ export const butuanEstablishments = [
   },
   {
     name: "Father Saturnino Urios University",
-    coords: "8.948121538520073, 125.54204786983483",
+    coords: "8.947734, 125.542197",
   },
   {
-    name: "Balay ni ED",
-    coords: "8.960950239553622, 125.50706852305585",
+    name: "Libertad",
+    coords: "8.944266, 125.503659",
+  },
+  {
+    name: "Bancasi",
+    coords: "8.947153, 125.476553",
+  },
+  {
+    name: "LMX Convention Center",
+    coords: "8.942445, 125.491466",
+  },
+  {
+    name: "Ideal Homes",
+    coords: "8.941047, 125.495700",
+  },
+  {
+    name: "Toyota Butuan City",
+    coords: "8.941854, 125.497407",
+  },
+  {
+    name: "Uruya Memorial Gardens",
+    coords: "8.937321, 125.484142",
+  },
+  {
+    name: "Sto. Nino Diocesan Shrine",
+    coords: "8.943848, 125.505628",
+  },
+  {
+    name: "Police Regional Office 13",
+    coords: "8.944733, 125.504965",
+  },
+  {
+    name: "Agusan Del Norte Provincial Hospital",
+    coords: "8.947626, 125.505254",
+  },
+  {
+    name: "Libertad Public Cemetery",
+    coords: "8.947235, 125.502819",
+  },
+  {
+    name: "Paradise Libertad Butuan City",
+    coords: "8.951025, 125.498203",
+  },
+  {
+    name: "Butuan City Jail",
+    coords: "8.950570, 125.504659",
+  },
+  {
+    name: "Agusan del Norte Provincial Jail",
+    coords: "8.950969, 125.505062",
+  },
+  {
+    name: "Father Saturnino Urios University - Morelos Campus",
+    coords: "8.952346, 125.502646",
+  },
+  {
+    name: "Department of Environment and Natural Resources (DENR) CARAGA Region",
+    coords: "8.956627, 125.506797",
+  },
+  {
+    name: "Ambago",
+    coords: "8.960773, 125.506895",
+  },
+  {
+    name: "Environmental Mgt. Bureau R-13",
+    coords: "8.963352, 125.506335",
+  },
+  {
+    name: "Emenvil Subdivision",
+    coords: "8.960555, 125.500523",
+  },
+  {
+    name: "Lumbocan",
+    coords: "9.003050, 125.502424",
+  },
+  {
+    name: "Port of Masao",
+    coords: "9.011424, 125.503654",
+  },
+  {
+    name: "Masao",
+    coords: "8.997016, 125.486976",
+  },
+  {
+    name: "Masao Beach",
+    coords: "8.999485, 125.484587",
+  },
+  {
+    name: "Doongan",
+    coords: "8.959172, 125.524014",
+  },
+  {
+    name: "National Telecommunications Commission",
+    coords: "8.959172, 125.524014",
+  },
+  {
+    name: "BJMP CARAGA Regional Office 13",
+    coords: "8.955008, 125.524236",
+  },
+  {
+    name: "DOH Warehouse",
+    coords: "8.954545, 125.524836",
+  },
+  {
+    name: "City Transportation and Traffic Management Department (CTTMD)",
+    coords: "8.955393, 125.527464",
+  },
+  {
+    name: "Balanghai Hotel and Convention Center",
+    coords: "8.953497, 125.527750",
+  },
+  {
+    name: "Langihan Public Market",
+    coords: "8.958188, 125.533656",
+  },
+  {
+    name: "Butuan City Integrated Bus Terminal",
+    coords: "8.959301, 125.535403",
+  },
+  {
+    name: "Holy Redeemer",
+    coords: "8.958177, 125.530308",
+  },
+  {
+    name: "Obrero",
+    coords: "8.962276, 125.537902",
+  },
+  {
+    name: "Butuan Tabuan",
+    coords: "8.959179, 125.535085",
+  },
+  {
+    name: "Bading",
+    coords: "8.969354, 125.535340",
+  },
+  {
+    name: "Agusan Peqeuño",
+    coords: "8.980911, 125.522698",
+  },
+  {
+    name: "Pagatpatan",
+    coords: "8.990591, 125.526161",
+  },
+  {
+    name: "RC Intino Building 2",
+    coords: "8.957030, 125.539865",
+  },
+  {
+    name: "Ong Yui",
+    coords: "8.955114, 125.540523",
+  },
+  {
+    name: "Watergate Hotel Butuan",
+    coords: "8.951040, 125.529933",
+  },
+  {
+    name: "Amarah Hotel",
+    coords: "8.951196, 125.529886",
+  },
+  {
+    name: "DPWH Regional Office 13 - CARAGA",
+    coords: "8.950038, 125.530325",
+  },
+  {
+    name: "Department of Labor and Employment ",
+    coords: "8.950480, 125.530137",
+  },
+  {
+    name: "Butuan City Library",
+    coords: "8.950693, 125.530054",
+  },
+  {
+    name: "Department of Information and Communications Technology - Caraga Region",
+    coords: "8.949929, 125.529265",
+  },
+  {
+    name: "Overseas Workers Welfare Administrarion - Regional Welfare Office CARAGA",
+    coords: "8.949929, 125.529265",
+  },
+  {
+    name: "Boy Scout of the Philippines (Agusan Council)",
+    coords: "8.948328, 125.530943",
+  },
+  {
+    name: "Butuan City Water District",
+    coords: "8.947653, 125.531192",
+  },
+  {
+    name: "Butuan City School of Arts & Trades",
+    coords: "8.947653, 125.531192",
+  },
+  {
+    name: "Bureau of Internal Revenue Caraga Regional Office",
+    coords: "8.947178, 125.531346",
+  },
+  {
+    name: "Almont Inland Resort",
+    coords: "8.944977, 125.530129",
+  },
+  {
+    name: "Philippine Statistics Authority",
+    coords: "8.944555, 125.529081",
+  },
+  {
+    name: "Hotel Oazis",
+    coords: "8.944168, 125.527028",
+  },
+  {
+    name: "Department of Trade and Industry- Agusan del Norte Provincial Office",
+    coords: "8.944173, 125.526866",
+  },
+  {
+    name: "Samping Avenue",
+    coords: "8.943891, 125.525279",
+  },
+  {
+    name: "Butuan Doctors Hospital",
+    coords: "8.943504, 125.522591",
+  },
+  {
+    name: "Agusan del Norte Electric Cooperative, Inc.",
+    coords: "8.943353, 125.521846",
+  },
+  {
+    name: "Sports Complex",
+    coords: "8.943365, 125.507812",
+  },
+  {
+    name: "Bayanihan",
+    coords: "8.940117, 125.524668",
+  },
+  {
+    name: "Grand Palace Hotel",
+    coords: "8.940787, 125.531841",
+  },
+  {
+    name: "Agusan del Norte Provincial Capitol",
+    coords: "8.941308, 125.533502",
+  },
+  {
+    name: "DepED CARAGA",
+    coords: "8.943432, 125.532804",
+  },
+  {
+    name: "LTO - Butuan City",
+    coords: "8.943707, 125.532699",
+  },
+  {
+    name: "SJIT Annex Building",
+    coords: "8.950993, 125.539316",
+  },
+  {
+    name: "Saint Joseph Institute of Technology",
+    coords: "8.951293, 125.541016",
+  },
+  {
+    name: "Flor-Al Mansion-Butuan City Branch",
+    coords: "8.947756, 125.539515",
+  },
+  {
+    name: "URIOS Gym",
+    coords: "8.947503, 125.541602",
+  },
+  {
+    name: "Manuel J. Santos Hospital",
+    coords: "8.947063, 125.540710",
+  },
+  {
+    name: "Butuan Central Elementary School",
+    coords: "8.946561, 125.543481",
+  },
+  {
+    name: "Butuan City Special Education Center - SPED",
+    coords: "8.945373, 125.543403",
+  },
+  {
+    name: "Agusan National High School",
+    coords: "8.944563, 125.543355",
+  },
+  {
+    name: "DOH - CHD Caraga Regional Office",
+    coords: "8.943532, 125.539495",
+  },
+  {
+    name: "Holy Child Colleges of Butuan Hospital",
+    coords: "8.941614, 125.537675",
+  },
+  {
+    name: "Golden Ribbon",
+    coords: "8.939241, 125.541855",
+  },
+  {
+    name: "Agao",
+    coords: "8.943818, 125.544997",
+  },
+  {
+    name: "National Security System",
+    coords: "8.944802, 125.543401",
+  },
+  {
+    name: "Guingona Park - Butuan Plaza",
+    coords: "8.947388, 125.543409",
+  },
+  {
+    name: "Almont City Hotel",
+    coords: "8.948062, 125.543250",
+  },
+  {
+    name: "Butuan Maternity Hospital and Puericulture Center",
+    coords: "8.947997, 125.545153",
+  },
+  {
+    name: "Butuan City Post and Telecon Offices",
+    coords: "8.951772, 125.542611",
+  },
+  {
+    name: "Philippine Ports Authority - Port Management Office/Agusan del Norte",
+    coords: "8.952464, 125.542410",
+  },
+  {
+    name: "Butuan Southville Hotel & Convention Center",
+    coords: "8.923938, 125.540143",
+  },
+  {
+    name: "Sprinkles Water Park and Event Center",
+    coords: "8.922244, 125.528883",
+  },
+  {
+    name: "San Vicente",
+    coords: "8.905277, 125.547407",
+  },
+  {
+    name: "Maon",
+    coords: "8.933279, 125.546142",
+  },
+  {
+    name: "Baan Riverside",
+    coords: "8.951147, 125.548587",
+  },
+  {
+    name: "Banza",
+    coords: "8.979915, 125.545323",
+  },
+  {
+    name: "Banza Church Ruins",
+    coords: "8.972063, 125.538613",
+  },
+  {
+    name: "Maug",
+    coords: "8.981325, 125.535661",
+  },
+  {
+    name: "Buhangin",
+    coords: "8.946423, 125.548771",
+  },
+  {
+    name: "Suzuki Auto Butuan",
+    coords: "8.952894, 125.558612",
+  },
+  {
+    name: "Nissan Butuan",
+    coords: "8.953800, 125.563973",
+  },
+  {
+    name: "CitiHardware Baan",
+    coords: "8.955705, 125.574595",
+  },
+  {
+    name: "Wilcon Depot Butuan",
+    coords: "8.956016, 125.576413",
+  },
+  {
+    name: "CitiHardware Butuan",
+    coords: "8.943224, 125.520355",
+  },
+  {
+    name: "CitiHardware Bancasi",
+    coords: "8.950369, 125.468521",
+  },
+  {
+    name: "Tiniwisan Crossing",
+    coords: "8.957314, 125.582101",
+  },
+  {
+    name: "Alviola Village",
+    coords: "8.957698, 125.585725",
+  },
+  {
+    name: "Butuan Medical Center",
+    coords: "8.962443, 125.585270",
+  },
+  {
+    name: "Saint Peter College Seminary",
+    coords: "8.958845, 125.591840",
+  },
+  {
+    name: "AllHome Butuan",
+    coords: "8.958928, 125.592529",
+  },
+  {
+    name: "Butuan Polysports Complex",
+    coords: "8.967094, 125.591347",
+  },
+  {
+    name: "National Bureau of Investigation - CARAGA Regional Office",
+    coords: "8.968065, 125.592885",
+  },
+  {
+    name: "Philippine Science High School - Caraga Region Campus",
+    coords: "8.969719, 125.593465",
+  },
+  {
+    name: "Commission on Higher Education - CARAGA",
+    coords: "8.959770, 125.597369",
+  },
+  {
+    name: "Ampayon",
+    coords: "8.961657, 125.602415",
+  },
+  {
+    name: "LTFRB Caraga Regional Office",
+    coords: "8.956718, 125.611939",
+  },
+  {
+    name: "Antongalon",
+    coords: "8.949553, 125.620519",
+  },
+  {
+    name: "Taligaman",
+    coords: "8.940439, 125.629096",
+  },
+  {
+    name: "Sumilihon",
+    coords: "8.991607, 125.612787",
+  },
+  {
+    name: "Taguibo",
+    coords: "8.977994, 125.624301",
+  },
+  {
+    name: "Los Angeles",
+    coords: "9.014035, 125.606319",
+  },
+  {
+    name: "Department of Trade and industry Caraga Regional Office",
+    coords: "8.946272, 125.533409",
+  },
+  {
+    name: "DPWH Butuan City DEO",
+    coords: "8.944592, 125.536246",
   },
 ];
